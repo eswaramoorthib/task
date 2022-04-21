038e7ff5d1888d18d0866d16771cae9e9cb7c3e3
Balasubramani <eswar.balasubramani@acsicorp.com>
2022-04-21 11:07:03 +0530

    associations changed

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,11 +1,15 @@
 class Person < ApplicationRecord
-    belongs_to :parent, class_name: "Person", optional: true
 
-    def children_name
-        Person.where(parent: self)
-    end
-    def grandchildren
-        Person.where(parent_id: children_name.pluck(:id))
-    end
-    
+    has_many :children, foreign_key: "parent_id", class_name: "Person"
+    belongs_to :parent, class_name: "Person", optional: true
+    has_many :grandchildren, through: :children, source: :children
+    # belongs_to :person, through: :children
+
+    # def children_name
+    #     Person.where(parent: self)
+    # end
+    # def grandchildrens
+    #     Person.where(parent_id: children_name.pluck(:id))
+    # end
+        
 end
